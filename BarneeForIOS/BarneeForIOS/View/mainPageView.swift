7ae3a2d0032efffdf6a05a578abc4bb6a50dca05
Nestertsov Maksym <nestertsovmaxim@gmail.com>
2023-06-19 20:14:43 +1000

    add pods and start creating VIPER

---
+++ BarneeForIOS/BarneeForIOS/View/mainPageView.swift
@@ -0,0 +1,13 @@
+//
+//  mainPageView.swift
+//  BarneeForIOS
+//
+//  Created by M1 on 19.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var logoLabel: UILabel!
+    
+}
---
+++ BarneeForIOS/Pods/Target Support Files/Pods-BarneeForIOS/Pods-BarneeForIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BarneeForIOS : NSObject
+@end
+@implementation PodsDummy_Pods_BarneeForIOS
+@end
